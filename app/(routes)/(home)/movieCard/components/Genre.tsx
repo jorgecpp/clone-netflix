03a724e9121f68fb08a96dcdf4231bfd08e018e4
jorgecpp <jorgetevescpp@gmail.com>
2026-04-05 20:53:51 -0500

    corriendo errores

--- app/(routes)/(home)/movieCard/components/Genre.tsx
+++ app/(routes)/(home)/movieCard/components/Genre.tsx
@@ -1,14 +1,14 @@
 
 type Props = {
-    genre: String[]
+    genre: string[]
 }
 
 export function Genre({genre}:Props){
     return(
         <div className="flex gap-3 ">
             {
-                genre.map(gen=>(
-                    <div key={gen} className="text-white text-xl">{gen}</div>
+                genre.map((gen,index)=>(
+                    <div key={index} className="text-white text-xl">{gen}</div>
                 ))
             }
         </div>
